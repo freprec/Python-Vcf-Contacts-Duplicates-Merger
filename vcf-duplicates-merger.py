bb06dfe93e79fa309ada145df46dd9f6cf93c526
freprec <58403266+freprec@users.noreply.github.com>
2020-07-31 18:42:37 +0200

    Identical names and ambiguity warnings
    Function added to check for identical names or organizations.
    Function added to print warnings for ambiguities on merged contacts.

--- vcf-duplicates-merger.py
+++ vcf-duplicates-merger.py
@@ -8,12 +8,13 @@ VERSION:3.0
 EMAIL;TYPE=INTERNET:jeffrey@osafoundation.org
 FN:Jeffrey Harris
 N:Harris;Jeffrey;;;
+ORG:Wk
 END:VCARD
 BEGIN:VCARD
 VERSION:3.0
 EMAIL;TYPE=INTERNET:jeffrey@osafoundation.org
-FN:Jeffrey Arsch
-N:Arsch;Jeffrey;;;
+FN:Jeffrey Harris
+N:Harris;Jeffrey;;;
 END:VCARD
 """
 
@@ -21,14 +22,56 @@ vcardlist = []
 for vcard in vobject.readComponents( s ):
     vcardlist.append(vcard)
 
-vcard0 = vcardlist[0].serialize()
-vcard1 = vcardlist[1].serialize()
-
-vcardlist[0].prettyPrint()
-vcardlist[1].prettyPrint()
-
 # v = vobject.readOne( s.serialize )
 # v.prettyPrint()
 
 # v = vobject.readOne( s )
 # v.prettyPrint()
+
+# returns true if two vCards have the same names and organization
+def identical_name_check(vcard_a, vcard_b, ambiguity_warnings):
+    has_n_attr_a = hasattr(vcard_a, 'n')
+    has_fn_attr_a = hasattr(vcard_a, 'fn')
+    has_org_attr_a = hasattr(vcard_a, 'org')
+    
+    has_n_attr_b = hasattr(vcard_b, 'n')
+    has_fn_attr_b = hasattr(vcard_b, 'fn')
+    has_org_attr_b = hasattr(vcard_b, 'org')
+
+    ret = True
+    if (has_n_attr_a and has_n_attr_b):
+        ret = ret and (vcard_a.n.value == vcard_b.n.value)
+
+    if (has_fn_attr_a and has_fn_attr_b):
+        ret = ret and (vcard_a.fn.value == vcard_b.fn.value)
+        
+    if (has_org_attr_a and has_org_attr_b):
+        ret = ret and (vcard_a.org.value == vcard_b.org.value)
+
+    if (ret):
+        if (has_n_attr_a ^ has_n_attr_b):
+            ambiguity_warnings.append('Attribute N exists only once.\n')
+        if (has_fn_attr_a ^ has_fn_attr_b):
+            ambiguity_warnings.append('Attribute FN exists only once.\n')
+        if (has_org_attr_a ^ has_org_attr_b):
+            ambiguity_warnings.append('Attribute ORG exists only once.\n')
+    
+    return ret
+
+# prints warning messages and vCards for the user to check if they are being correctly merged
+def print_ambiguity_warning(ambiguity_warnings, vcard_a, vcard_b):
+    print ('--- Merged with {} warning(s) ---'.format(len(ambiguity_warnings)))
+    for line in ambiguity_warnings:
+        print (line)
+    print (vcard_a.serialize())
+    print (' --and-- \n')
+    print (vcard_b.serialize())
+
+
+
+
+
+ambiguity_warnings = []
+is_identical_name = identical_name_check(vcardlist[0], vcardlist[1], ambiguity_warnings)
+if (len(ambiguity_warnings) > 0):
+    print_ambiguity_warning(ambiguity_warnings, vcardlist[0], vcardlist[1])
