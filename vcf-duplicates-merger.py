cb0fd706d61fdf48ed054d75896217ea561a2722
freprec <58403266+freprec@users.noreply.github.com>
2020-08-03 00:29:10 +0200

    Successful output file
    Added functions to merge vCards and adding missing attributes from other vCards.
    Added output file.
    Problem with output file on windows: after every line an empty line is inserted. Works on Ubuntu.

--- vcf-duplicates-merger.py
+++ vcf-duplicates-merger.py
@@ -1,39 +1,18 @@
 import vobject
 
-disable_warnings_for_empty_contacts = True
+disable_warnings_for_empty_contacts = True  # only show warnings for vCards with more than a single attribute
 input_file_path = ""
-
-input_file = open(input_file_path, "r")
-
-# s = """
-# BEGIN:VCARD
-# VERSION:3.0
-# EMAIL;TYPE=INTERNET:jeffrey@osafoundation.org
-# FN:Jeffrey Harris
-# N:Harris;Jeffrey;;;
-# ORG:Wk
-# END:VCARD
-# BEGIN:VCARD
-# VERSION:3.0
-# EMAIL;TYPE=INTERNET:jeffrey@osafoundation.org
-# FN:Jeffrey Harris
-# N:Harris;Jeffrey;;;
-# END:VCARD
-# """
+output_file_path = "dupmerged_" + input_file_path
+#input_file = open(input_file_path, "r")
+#output_file = open(output_file_path, "w")
 
 vcardlist = []
-for vcard in vobject.readComponents( input_file.read() ):
-    vcardlist.append(vcard)
-
-# v = vobject.readOne( s.serialize )
-# v.prettyPrint()
-
-# v = vobject.readOne( s )
-# v.prettyPrint()
-
+with open(input_file_path, "rt") as input_file:
+    for vcard in vobject.readComponents( input_file.read() ):
+        vcardlist.append(vcard)
 
 # count attributes with actual content
-def vcard_content_counter(vcard):
+def count_vcard_content(vcard):
     content_counter = 0
     for attribute in vcard.contents:
         for item in vcard.contents[attribute]:
@@ -63,12 +42,12 @@ def identical_name_check(vcard_a, vcard_b, ambiguity_warnings):
 
     if (ret):
         # count attributes with actual content
-        content_counter_a = vcard_content_counter(vcard_a)
-        content_counter_b = vcard_content_counter(vcard_b)
+        content_counter_a = count_vcard_content(vcard_a)
+        content_counter_b = count_vcard_content(vcard_b)
 
         if ((not disable_warnings_for_empty_contacts) \
             or disable_warnings_for_empty_contacts \
-            and content_counter_a > 1 and content_counter_b > 1):
+            and content_counter_a > 1 and content_counter_b > 1):   # only show warnings for vCards with more than a single attribute
             if (has_n_attr_a ^ has_n_attr_b):
                 ambiguity_warnings.append('Attribute N exists only once.\n')
             if (has_fn_attr_a ^ has_fn_attr_b):
@@ -80,29 +59,67 @@ def identical_name_check(vcard_a, vcard_b, ambiguity_warnings):
 
 # prints warning messages and vCards for the user to check if they are being correctly merged
 def print_ambiguity_warning(ambiguity_warnings, vcard_a, vcard_b):
-    print ('--- Merged with {} warning(s) ---'.format(len(ambiguity_warnings)))
+    print('--- Merged with {} warning(s): ---'.format(len(ambiguity_warnings)))
     for line in ambiguity_warnings:
-        print (line)
-    print (vcard_a.serialize())
-    print (' --and-- \n')
-    print (vcard_b.serialize())
+        print(line)
+    print(vcard_a.serialize())
+    print(' --and-- \n')
+    print(vcard_b.serialize())
 
-# VObject requires the FN attribute to be present
+# add the FN attribute to the vCard if not present
 def add_fn_attribute(vcard):
     if (not hasattr(vcard, 'fn')):
         vcard.add('fn')
 
+# add an item to a vCard attribute
+def add_to_vcard_attribute(vcard, attribute, item):
+    vcard.add(attribute).value = item.value
+    value_index = len(vcard.contents[attribute]) - 1
+    vcard.contents[attribute][value_index].params = item.params
+
+# merge two vCards
+def merge_vcards(vcard_in, vcard_merge):
+    for attribute in vcard_in.contents:
+        if hasattr(vcard_merge, attribute):
+            for item in vcard_in.contents[attribute]:
+                if item not in vcard_merge.contents[attribute]:
+                    add_to_vcard_attribute(vcard_merge, attribute, item)
+        else:
+            for item in vcard_in.contents[attribute]:
+                add_to_vcard_attribute(vcard_merge, attribute, item)
+
+out_index = 0               # index counter for output of vCards
+merge_indices_dict = { }    # dictionary for tracking merged vCards: key - input index, value - output index
+out_vcards_list = []        # list for the remaining merged vCards
 
 for i in range(len(vcardlist)):
+    if i in merge_indices_dict:
+        continue    # vCard was already merged into out_vcards_list
+    else:
+        merge_indices_dict[i] = out_index       # set output index of vCard
+        out_vcards_list.append(vcardlist[i])    # add vCard to out_vcards_list
+
     for j in range(i + 1, len(vcardlist)):
+        if j in merge_indices_dict:
+            continue    # vCard was already merged into out_vcards_list
+
         ambiguity_warnings = []
         is_identical_name = identical_name_check(vcardlist[i], vcardlist[j], ambiguity_warnings)
+        # VObject requires the FN attribute to be present
         add_fn_attribute(vcardlist[i])
         add_fn_attribute(vcardlist[j])
-        if (is_identical_name and len(ambiguity_warnings) > 0):
-            print_ambiguity_warning(ambiguity_warnings, vcardlist[i], vcardlist[j])
+        if (is_identical_name):
+            merge_indices_dict[j] = out_index # set output index of vCard
+            # merge the vCards into out_vcards_list
+            merge_vcards(vcardlist[j], out_vcards_list[out_index])
 
-# ambiguity_warnings = []
-# is_identical_name = identical_name_check(vcardlist[0], vcardlist[1], ambiguity_warnings)
-# if (len(ambiguity_warnings) > 0):
-#     print_ambiguity_warning(ambiguity_warnings, vcardlist[0], vcardlist[1])
+            if (len(ambiguity_warnings) > 0):
+                print_ambiguity_warning(ambiguity_warnings, vcardlist[i], vcardlist[j])
+    
+    out_index += 1
+
+# write the vCards to the output file
+with open(output_file_path, "wt") as output_file:
+    for vcard in out_vcards_list:
+        serial_vcard = vcard.serialize()
+        output_file.write(vcard.serialize())
